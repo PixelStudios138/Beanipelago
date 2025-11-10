60ad03a91d863d440a320f7643a003873afe094c
PixelStudios138 <marleybroadbent@gmail.com>
2025-11-10 11:04:48 +1100

    Initial commit

---
+++ Assets/Scripts/LevelLoaders/LoadNextScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextSceney()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
